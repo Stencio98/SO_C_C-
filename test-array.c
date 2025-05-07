a1be9faea65f2dbcc556e009ae9a4f07c59262e4
Your Name <you@example.com>
2025-05-07 23:58:23 +0200

    .

--- test-array.c
+++ test-array.c
@@ -8,23 +8,19 @@ int main() {
 
     // Attempting to use a.length --> invalid in C, so comment
     // printf("a.length --> NO in C\n");
-//0 15 size_t
-//-8 7 int
+    //0 15 size_t
+    //-8 7 int
     // Size of the array in bytes
     size_t size_of_a = sizeof(a);
     printf("Size of array a in bytes: %zu\n", size_of_a);
-
     // Calculate the number of elements in the array
     size_t length_a = sizeof(a) / sizeof(a[0]);
     printf("Number of elements in array a: %zu\n", length_a);
 
-    // Optional: Add assertions or checks
-    /*
-    if (length_a == 10) {
-        printf("Test passed: Array length is 10.\n");
-    } else {
-        printf("Test failed: Array length is not 10.\n");
-    }*/
+    size_t size_of_b = sizeof(b);
+    printf("Size of array a in bytes: %zu\n", size_of_b);
+    size_t length_b = sizeof(b) / sizeof(b[0]);
+    printf("Number of elements in array a: %zu\n", length_b);
 
     return 0;
 }
