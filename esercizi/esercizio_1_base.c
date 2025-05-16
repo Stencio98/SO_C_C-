de911f24760488752866d3a93557bf7661e05028
Stencio <stefano.monal.98@gmail.com>
2025-05-16 10:48:02 +0200

    M

--- esercizi/esercizio_1_base.c
+++ esercizi/esercizio_1_base.c
@@ -5,7 +5,7 @@
 //#define SEGV --> macro
 // gcc -D SEGV esercizio_1_base.c
 int main(){
-    int a[10];
+    int a[10]; // int a[10] = {1} --> all a[i] = 1
     int i = 0;
     int sum = 0;
     int len_a = sizeof(a) / sizeof(a[0]);
@@ -20,7 +20,8 @@ int main(){
         i++;
     }
     printf("sum: %d\n", sum);
-    printf("maybe strange number, why?\n\n\n");
+    printf("maybe strange number, why?\n");
+    printf("we have not inizialized the a array, so we read the values that were there before\n\n\n\n");
 
 
 
@@ -34,8 +35,8 @@ int main(){
         i++;
     }
     printf("sum: %d\n", sum);
-    printf("maybe strange number, why?\n\n\n");
-
+    printf("maybe strange number, why?\n");
+	printf("overflow clock mechanism\n\n\n\n");
 
 
     i = 0;
@@ -47,7 +48,7 @@ int main(){
         i++;
     }
     printf("sum: %d\n", sum);
-    printf("finally normal exercise\n\n\n");
+    printf("finally normal exercise\n\n\n\n");
 
 
 
@@ -57,7 +58,25 @@ int main(){
         printf("a[%d]: %d\n", i, *(p + i));
         i++;
     }
-    printf("array a has been read with pointer *p\n\n\n");
+    printf("array a has been read with pointer *p, pointers work similar to arrays\n\n\n\n");
+	
+	
+	
+	printf("string with different ways to print:\n");
+	printf("print with printf and %%:\n");
+	char str[/*20*/] = "tung tung tung Saur";
+	printf("str: %s\n\a", str); // \a allarme(campanella / beep)
+	
+	printf("str printed with pointers:\n");
+	char * pc;
+	pc = str;
+	printf("str: ");
+	while(*pc != '\0'){
+		printf("%c", *pc);
+		pc++; // like i in array
+	}
+	printf("\n"); // end string so we print newline
+
 
     return 0;
 }
